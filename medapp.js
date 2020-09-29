492025b689d9fe59f17d4f3e26d7f86a819b4dec
dhdorr <dhdorr@gmail.com>
2020-09-29 01:00:49 -0700

    dropdown menus
    made desktop view drop downs, working on mobile dropdowns

--- medapp.js
+++ medapp.js
@@ -1,6 +1,10 @@
+const navText = document.querySelectorAll(".nav-text");
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav-links");
+const subNav = document.querySelector(".sub-nav");
+
+//Navbar slide out for mobile view
 const navSlide = () => {
-  const hamburger = document.querySelector(".hamburger");
-  const nav = document.querySelector(".nav-links");
   //Toggle Nav
   hamburger.addEventListener("click", () => {
     nav.classList.toggle("nav-active");
@@ -8,11 +12,37 @@ const navSlide = () => {
     //Hamburger Animation
     hamburger.classList.toggle("toggle");
   });
+
+  //Toggle Nav
+  navText.forEach((element) => {
+    element.addEventListener("click", () => {
+      nav.classList.toggle("nav-active");
+
+      //Hamburger Animation
+      hamburger.classList.toggle("toggle");
+    });
+  });
+};
+
+//Navbar slide out for desktop view
+const navExtender = () => {
+  navText.forEach((element) => {
+    element.addEventListener("mouseenter", () => {
+      console.log(element);
+      //subNav.style.setProperty("display", "block");
+    });
+
+    element.addEventListener("mouseout", () => {
+      console.log(element);
+      //subNav.style.setProperty("display", "none");
+    });
+  });
 };
 
 //Mother function that calls child functions
 const medapp = () => {
   navSlide();
+  navExtender();
 };
 
 medapp();
